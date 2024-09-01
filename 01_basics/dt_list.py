bfb7f5ceaf7f2adccb7e3bebe6e17e58cba7064e
shoukatalipec <shoukatali.pec@gmail.com>
2024-09-01 21:50:30 +0500

    Complete list

--- 01_basics/dt_list.py
+++ 01_basics/dt_list.py
@@ -61,3 +61,13 @@ tea_var_c.append('Red Tea')
 print("After Appending:")
 print("tea_var\t\t:",tea_var)
 print("tea_var_c\t:",tea_var_c)
+
+# List comprehension
+squared_num = [x for x in range(10)]
+print(squared_num)
+
+square_num = [x**2 for x in range(10)]
+print(square_num)
+
+cube_num = [y**3 for y in range(10)]
+print(cube_num)
