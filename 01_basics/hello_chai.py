0f9c8fb98ed6411df05322783cbc719527409211
shoukatalipec <shoukatali.pec@gmail.com>
2024-09-01 12:20:08 +0500

    First file to commit

--- 01_basics/hello_chai.py
+++ 01_basics/hello_chai.py
@@ -1,3 +1,5 @@
 from chai import chai
+from types_of_python import types_of_python
 
 chai("ginger tea")
+types_of_python()
--- 01_basics/types_of_python.py
+++ 01_basics/types_of_python.py
@@ -1,5 +1,7 @@
-print("Types of python")
-print("\n1- Mutable")
-print("-> List\n-> Set\n-> Dictionary\n-> Bytearray\n-> Array")
-print("\n1- Immutable")
-print("-> Integer\n-> Floating Point\n-> Boolean\n-> Strings\n-> Typples\n-> Frozen Set\n-> Bytes")
+
+def types_of_python():
+  print("Types of python")
+  print("\n1- Mutable")
+  print("-> List\n-> Set\n-> Dictionary\n-> Bytearray\n-> Array")
+  print("\n1- Immutable")
+  print("-> Integer\n-> Floating Point\n-> Boolean\n-> Strings\n-> Typples\n-> Frozen Set\n-> Bytes")
