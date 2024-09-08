bd61687d096251fcf91fa413d5d12ac80722166a
shoukatalipec <shoukatali.pec@gmail.com>
2024-09-08 20:07:11 +0500

    api-handling-more practice

---
+++ 11_api_handling/api_handling_joke_id.py
@@ -0,0 +1,23 @@
+import requests
+
+def fetch_api(joke_id):
+  url = f"https://api.freeapi.app/api/v1/public/randomjokes/{joke_id}"
+  response = requests.get(url)
+  data = response.json()
+
+  if data["success"] and "data" in data:
+    user_data = data["data"]
+    joke = user_data["content"]
+    return joke
+  else:
+    raise Exception("Failed to fetch user data.")
+
+def main():
+  try:
+    joke = fetch_api(12)
+    print(f"{joke}")
+  except Exception as e:
+    print(str(e))
+
+if __name__ == "__main__":
+  main()
